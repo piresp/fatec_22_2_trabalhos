a3d06a0e121045e694f24cdc08f6613f3f61139f
Gabriel Pires <60672959+piresp@users.noreply.github.com>
2022-09-02 00:50:46 -0300

    Update estrutura_de_dados_01.c

--- estrutura_de_dados_01.c
+++ estrutura_de_dados_01.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include <locale.h>
 
-#define YEARS 18
-
 typedef struct {
 	int age;
 	char name[60];
@@ -14,12 +12,12 @@ typedef struct {
 
 typedef struct {
 	int region;
-	char capital[30];
+	const char* capital;
 } Destiny;
 
 typedef struct {
 	int region;
-	char capital[30];
+	const char* capital;
 } Origin;
 
 typedef struct {
@@ -30,37 +28,37 @@ typedef struct {
 } Order;
 
 enum All_regions { // REGION
-	Sul=1, Sudeste, CentroOeste, Nordeste, Norte, LastRegion
+    Sul=1, Sudeste, CentroOeste, Nordeste, Norte, LastRegion
 } region;
 
-const char* regionA[] = {"SUL", "SUDESTE", "CENTRO-OESTE", "NORDESTE", "NORTE"};
+const char* regionA[] = {"SUL", "SUDESTE", "CENTRO-OESTE", "NORDESTE", "NORTE", 0};
 
 typedef enum { // SUL
-	Curitiba=1, Florianopolis, RioGrandeDoSul ,LastSouth
+    Curitiba=1, Florianopolis, RioGrandeDoSul ,LastSouth
 } south;
 
 const char* southA[] = {"CURITIBA", "FLORIANÓPOLIS", "RIO GRANDE DO SUL", 0};
 
 typedef enum { // SUDESTE
-	SaoPaulo=1, BeloHorizonte, RioDeJaneiro, Vitoria, LastSouthEast
+    SaoPaulo=1, BeloHorizonte, RioDeJaneiro, Vitoria, LastSouthEast
 } southeast;
 
 const char* southeastA[] = {"SÃO PAULO", "BELO HORIZONTE", "RIO DE JANEIRO", "VITÓRIA", 0};
 
 typedef enum { // CENTRO OESTE
-	Brasilia, Goiania, Cuiaba, CampoGrande, LastMidWest
+    Brasilia, Goiania, Cuiaba, CampoGrande, LastMidWest
 } midwest;
 
 const char* midwestA[] = {"BRASÍLIA", "GOIANIA", "CUIABA", "CAMPO GRANDE", 0};
 
 typedef enum { // NORDESTE
-	Salvador, SaoLuis, Teresina, Fortaleza, Natal, JoaoPessoa, Recife, Maceio, Aracaju, LastNortheast
+    Salvador, SaoLuis, Teresina, Fortaleza, Natal, JoaoPessoa, Recife, Maceio, Aracaju, LastNortheast
 } northeast;
 
 const char* northeastA[] = {"SALVADOR", "SAO LUÍS", "TERESINA", "FORTALEZA", "NATAL", "JOÃO PESSOA", "RECIFE", "MACEIÓ", "ARACAJU", 0};
 
 typedef enum { // NORTE
-	RioBranco, Macapa, Manaus, Belem, PortoVelho, BoaVista, Palmas, LastNorth
+    RioBranco, Macapa, Manaus, Belem, PortoVelho, BoaVista, Palmas, LastNorth
 } north;
 
 const char* northA[] = {"RIO BRANCO", "MACAPÁ", "MANAUS", "BELÉM", "PORTO VELHO", "BOA VISTA", "PALMAS", 0};
@@ -81,13 +79,14 @@ void display(const char* menu) {
 }
 
 void display_capitals(const char* capitals[]) {
-	
-	int size = sizeof(capitals);
-	
-	for(int i = 0; i < size; i++) {
+	int i;
+	while (capitals[i] != '\0') {
 		printf("\n%d - %s", i+1, capitals[i]);
+		i++;
 	}
+
 }
+
 
 void welcome() {
 	display("BEM VINDO!");
@@ -124,52 +123,52 @@ void end() {
 
 int read_where(int number) {
 	int confirm = 0;
-	
+
 	do {
 		puts("\n\nINSIRA NUMERO CORRESPONDENTE: ");
 		scanf("%d", &confirm);
 	} while (confirm <1 || confirm > number);
-	
+
 	return confirm;
 }
 
 Identity set_identity(Identity id) {
-	
+
 	display("CADASTRO: DADOS PESSOAIS");
-	
+
 	printf("NOME: ");
 	fgets(id.name, 100, stdin);
-	
+
 	printf("CPF: ");
 	fgets(id.cpf, 20, stdin);
-	
+
 	printf("IDADE: ");
 	scanf("%d", &id.age);
-	
+
 	return id;
 }
 
 Identity get_identity(Identity id) {
-	
+
 	break_line();
 	printf("IDENTIDADE:");
 	printf("\n\nNOME: %s",id.name);
 	printf("CPF: %s",id.cpf);
 	printf("IDADE: %d",id.age);
-	
+
 	break_line();
 }
 
 Origin set_origin_region(Origin origin) {
-	
+
 	display("ORIGEM: REGIÃO");
 	for(int i = 0; i< Norte; i++) {
 		printf("\n%d - %s", i+1, regionA[i]);
 	}
-	
+
 	int take_region = read_where(5);
 	origin.region = take_region;
-	
+
 	return origin;
 }
 
@@ -178,35 +177,60 @@ Origin get_origin_region(Origin origin) {
 	printf("%d - %s", origin.region, regionA[origin.region - 1]);
 }
 
+Origin body_set_origin_capital(Origin origin, const char** array) {
+
+	display_capitals(array);
+	int j = read_where(sizeof(array));
+	origin.capital = array[j - 1];
+
+	return origin;
+
+}
+
 Origin set_origin_capital(Origin origin) {
-	
+
 	display("ORIGEM: CAPITAL");
-	
+
 	switch (origin.region) {
-		case 1:
-			display_capitals(southA);
-			read_where(sizeof(southA));
+		case 1: {
+
+			origin = body_set_origin_capital(origin, southA);
+
+			return origin;
 			break;
-		
-		case 2:
-			display_capitals(southeastA);
-			read_where(sizeof(southeastA));
+		}
+
+		case 2: {
+
+			origin = body_set_origin_capital(origin, southeastA);
+
+			return origin;
 			break;
-		
-		case 3:
-			display_capitals(midwestA);
-			read_where(sizeof(midwestA));
+		}
+
+		case 3: {
+
+			origin = body_set_origin_capital(origin, midwestA);
+
+			return origin;
 			break;
-		
-		case 4:
-			display_capitals(northeastA);
-			read_where(sizeof(northeastA));
+		}
+
+		case 4: {
+
+			origin = body_set_origin_capital(origin, northeastA);
+
+			return origin;
 			break;
-		
-		case 5:
-			display_capitals(northA);
-			read_where(sizeof(northA));
+		}
+
+		case 5: {
+
+			origin = body_set_origin_capital(origin, northA);
+			
+			return origin;
 			break;
+		}
 	}
 }
 
@@ -215,19 +239,18 @@ int main() {
 	setlocale(LC_ALL, "Portuguese");
 	/*
 	welcome();
-	
-	
+
 	Identity id;
 	id = set_identity(id);
 	get_identity(id);
-	
-	
-	;*/
+
+	*/
 	Origin ori;
 	ori = set_origin_region(ori);
 	// get_origin_region(ori);
-	
-	set_origin_capital(ori);
+
+	ori = set_origin_capital(ori);
+	// get_origin_capital();
 
 	return 0;
 }
